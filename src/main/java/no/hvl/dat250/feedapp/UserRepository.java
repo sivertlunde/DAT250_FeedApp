31e06bcf6f0a6264405700a5e176c8c495da288e
Aredae <aredaehlen@gmail.com>
2020-10-01 13:14:04 +0200

    Yo, add 1 line yall

---
+++ src/main/java/no/hvl/dat250/feedapp/UserRepository.java
@@ -0,0 +1,11 @@
+package no.hvl.dat250.feedapp;
+
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface UserRepository extends CrudRepository<User, Long> {
+
+	User findByUsername(String username);
+	
+}
