81df4eaab2bb28630ad0a33d6260840533c566e3
sivertlunde <sivert.lunde@gmail.com>
2020-10-01 13:12:03 +0200

    Updated Role.java

--- src/main/java/no/hvl/dat250/feedapp/Role.java
+++ src/main/java/no/hvl/dat250/feedapp/Role.java
@@ -16,6 +16,27 @@ public class Role {
 	@OneToMany(mappedBy = "role")
 	private String role;
 	
-	
+	public Role() {}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getRole() {
+		return role;
+	}
+
+	public void setRole(String role) {
+		this.role = role;
+	}
+
+	@Override
+	public String toString() {
+		return "Role [id=" + id + ", role=" + role + "]";
+	}
 
 }
