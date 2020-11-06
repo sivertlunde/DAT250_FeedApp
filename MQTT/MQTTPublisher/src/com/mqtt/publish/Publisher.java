557edc4b917744da16150db90857e9e0b8090f1e
Aredae <aredaehlen@gmail.com>
2020-11-06 16:37:47 +0100

    some things

--- MQTT/MQTTPublisher/src/com/mqtt/publish/Publisher.java
+++ MQTT/MQTTPublisher/src/com/mqtt/publish/Publisher.java
@@ -1,12 +1,10 @@
 package com.mqtt.publish;
-import java.io.BufferedInputStream;
 import java.io.BufferedReader;
-import java.io.DataInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
-import java.util.Iterator;
+import java.util.ArrayList;
 import java.util.List;
 
 import org.eclipse.paho.client.mqttv3.MqttClient;
@@ -16,8 +14,8 @@ public class Publisher {
 
 	public static void main(String[] args) {
 		
-		//String messageString = "";
-		List<Integer> oldlist;
+		String messageString = "";
+		List<Integer> oldlist = new ArrayList<>();
 		
 		while(true) {
 			try {
@@ -34,6 +32,7 @@ public class Publisher {
 	            			System.out.println("== START PUBLISHER ==");
 	            			URL url2 = new URL("http://localhost:8080/polls/result/"+newlist.get(i));
 	        	            HttpURLConnection con2 = (HttpURLConnection) url2.openConnection();
+	        	            messageString = FullResponseBuilder.getFullResponse(con2);
 		        			MqttClient client = new MqttClient("tcp://localhost:1883", MqttClient.generateClientId());
 		        			client.connect();
 		        			MqttMessage message = new MqttMessage();
@@ -45,12 +44,13 @@ public class Publisher {
 		        			client.close();
 		        			
 		        			System.out.println("== END PUBLISHER ==");
-		        			oldlist = newlist;
+		        			
 	            		}else {
-	            			
+	            		System.out.println("Poll with id: " + newlist.get(i) + " has already been sent");
 	            		}
 		            	
 		            }
+	            	oldlist = newlist;
 	            }else if(con.getResponseCode() == 204) {
 	            	System.out.println("Nothing to update");
 	            }else {
@@ -62,7 +62,7 @@ public class Publisher {
 	            /*
 	            
 	            */
-	            Thread.sleep(60000);
+	            Thread.sleep(1000);
 	        } catch (Exception e) {
 	            e.printStackTrace();
 	            
